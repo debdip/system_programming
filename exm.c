c388264b7caf4458333d209e5777f41f335726c0
debdip <suddiptadebnath@gmail.com>
2016-12-07 00:42:23 +0600

    new commit

---
+++ exm.c
@@ -0,0 +1,10 @@
+#include <unistd.h> /*fork declared here*/
+#include <stdio.h> /* printf declared here*/
+int main() {
+   int answer = 84 >> 1;
+   printf("Answer: %d", answer);
+   fork();
+   kill();
+   return 0;
+
+}
